5129f4ae04ca67e59bb097d65410ea1debf38253
ashiq73 <ashiq.buet73@gmail.com>
2023-11-05 16:34:53 +0600

    post service in serviceCollection

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
-
-
-
-
+//middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lxmrjjn.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,16 +24,28 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("printMamaDB").collection("services");
+
+    //POST A SERVICE
+    app.post("/api/mama/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,12 +53,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Print MAMA Server is Running");
+});
 
-app.get('/',(req,res)=>{
-    res.send('Print MAMA Server is Running')
-})
-
-
-app.listen(port,(req,res)=>{
-    console.log(`Print Mama Runing on this Port: ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`Print Mama Runing on this Port: ${port}`);
+});
